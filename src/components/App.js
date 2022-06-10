a8645f1f13dba6b7f3abc46007acf340aef50a4f
Luis <foreverluiscastro@gmail.com>
2022-06-10 18:39:04 -0400

    Completed all deliverables.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useEffect , useState } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+  const [id , setId] = useState("")
+  const [topping, setTopping] = useState("")
+  const [size, setSize] = useState("")
+  const [vegetarian, setVegetarian] = useState("")
+
+  const API = 'http://localhost:3001/pizzas'
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(pizzas => setPizzas(pizzas))
+  }, []);
+
+  function handleEditPizza(pizza) {
+    setTopping(pizza.topping)
+    setSize(pizza.size)
+    setVegetarian(pizza.vegetarian)
+    setId(pizza.id)
+  }
+
+  function submitEditedPizza(e) {
+    e.preventDefault()
+    // debugger
+    console.log(e.target)
+
+    const updatedPizza = {
+      id: id,
+      topping: e.target[0].value,
+      size: e.target[1].value,
+      vegetarian: e.target[2].value
+    }
+    // debugger
+    const configObj = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(updatedPizza)
+    }
+    fetch(API + `/${id}`, configObj)
+    .then((r) => r.json())
+    .then((patchedPizza) => {
+      const updatedList = pizzas.filter((pizza) => pizza.id != patchedPizza.id)
+      updatedList.push(patchedPizza)
+      const santizedArray = updatedList.sort((prevObj, currentObj) => {
+        return prevObj.id - currentObj.id
+      })
+      setPizzas(santizedArray)
+      setId("")
+      setTopping("")
+      setSize("")
+      setVegetarian("")
+    })
+    
+  }
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm onSubmitPizza={submitEditedPizza} topping={topping} setTopping={setTopping} size={size} setSize={setSize} vegetarian={vegetarian}/>
+      <PizzaList pizzas={pizzas} onEditPizza={handleEditPizza}/>
     </>
   );
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-function PizzaForm() {
+function PizzaForm({ topping , setTopping , setSize , size , vegetarian , onSubmitPizza }) {
+
+  function handleToppingChane(e) {
+    setTopping(e.target.value)
+  }
+
+  function handleSizeChange(e) {
+    setSize(e.target.value)
+  }
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={onSubmitPizza}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -10,10 +19,12 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            value={topping}
+            onChange={handleToppingChane}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select className="form-control" name="size" value={size} onChange={handleSizeChange}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -26,6 +37,7 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              checked={vegetarian === "" ? "" : vegetarian}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -35,6 +47,7 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={vegetarian === "" ? "" : !vegetarian}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
